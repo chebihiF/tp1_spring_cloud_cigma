36ef881b74532c1a6e5a3d46200d14ecb28f50a0
Chebihi <chebihi.infinitysoft@google.com>
2022-03-27 17:39:12 +0000

    add auto configuration via discovery client

--- gateway-service/src/main/java/cigma/glwa/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/cigma/glwa/gatewayservice/GatewayServiceApplication.java
@@ -2,6 +2,9 @@ package cigma.glwa.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -13,11 +16,19 @@ public class GatewayServiceApplication {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
-    @Bean
+    //@Bean
     RouteLocator routeLocator(RouteLocatorBuilder builder){
         return builder.routes()
-                .route((r)->r.path("/prodcuts/**").uri("lib://PRODUCT-SERVICE"))
+                .route((r)->r.path("/products/**").uri("lb://PRODUCT-SERVICE"))
                 .build();
+    }
+
+    @Bean
+    DiscoveryClientRouteDefinitionLocator definitionLocator(
+            ReactiveDiscoveryClient rdc,
+            DiscoveryLocatorProperties properties
+    ){
+        return new DiscoveryClientRouteDefinitionLocator(rdc,properties);
     }
 
 }
